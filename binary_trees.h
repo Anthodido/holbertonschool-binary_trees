f9510462e919c891222627f2cd670d4732d0ab73
root <root@ip-10-42-111-36.ec2.internal>
2025-12-08 14:13:23 +0000

    je passe par le fils gauche puis par le droit, je traverse l'arbre

--- binary_trees.h
+++ binary_trees.h
@@ -49,6 +49,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 void binary_tree_print(const binary_tree_t *tree);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
 
