0c2394c87f79e397189c21391cb3e007b66bfd40
root <root@ip-10-42-217-84.ec2.internal>
2025-12-09 14:10:55 +0000

    je verifie si toutes les branch sont parfaites

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - Vérifie si un arbre binaire est parfait
+ * @tree: pointeur vers la racine de l'arbre à vérifier
+ *
+ * Return: 1 si l'arbre est parfait, 0 sinon
+ */
+ 
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    size_t h_g, h_d;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+    if (binary_tree_is_full(tree) == 0)
+    {
+        return (0);
+    }
+    h_g = binary_tree_height(tree->left);
+    h_d = binary_tree_height(tree->right);
+
+    if (h_d != h_g)
+    {
+        return (0);
+    }
+    return (1);
+}
