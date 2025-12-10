409d212831ea80884e8719d6311ed7cf23cc6d9d
root <root@ip-10-42-44-82.ec2.internal>
2025-12-10 08:53:18 +0000

    reglage des problemes betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,33 +6,33 @@
  * Return: 1 si l'arbre est plein, 0 sinon
  */
 int binary_tree_is_full(const binary_tree_t *tree)
-{ 
-    int g_full;
-    int d_full;
+{
+	int g_full;
+	int d_full;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    if (tree->right == NULL && tree->left == NULL)
-    {
-        return (1);
-    }
-    if (tree->left != NULL && tree->right == NULL)
-    {
-        return (0);
-    }
-    if (tree->left == NULL && tree->right != NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		return (1);
+	}
+	if (tree->left != NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right != NULL)
+	{
+		return (0);
+	}
 
-    g_full = binary_tree_is_full(tree->left);
-    d_full = binary_tree_is_full(tree->right);
-    
-    if (g_full == 1 && d_full == 1)
-    {
-        return (1);
-    }
-    return (0);
+	g_full = binary_tree_is_full(tree->left);
+	d_full = binary_tree_is_full(tree->right);
+
+	if (g_full == 1 && d_full == 1)
+	{
+		return (1);
+	}
+	return (0);
 }
