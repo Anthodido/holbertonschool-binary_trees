07c7946956f55344dd6a0a6a409da4ab25eb39ed
root <root@ip-10-42-44-82.ec2.internal>
2025-12-10 09:34:59 +0000

    rine de logique

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,31 @@
 #include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left_height = 0;
+	right_height = 0;
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height);
+
+	return (right_height);
+}
+
 /**
  * binary_tree_is_perfect - Vérifie si un arbre binaire est parfait
  * @tree: pointeur vers la racine de l'arbre à vérifier
