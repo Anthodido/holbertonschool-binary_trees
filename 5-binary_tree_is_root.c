d0838875600e96013a0ddb6f35ce1b2a121f2842
root <root@ip-10-42-44-82.ec2.internal>
2025-12-10 08:44:04 +0000

    reglage des problemes betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,16 +6,16 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        if (node->parent == NULL)
-        {
-            return (1);
-        }
-    }
-    return (0);
+	if (node == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (node->parent == NULL)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
