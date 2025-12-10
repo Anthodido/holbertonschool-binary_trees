c16b81d71901c5edbca27d16395626d839ca60cc
root <root@ip-10-42-44-82.ec2.internal>
2025-12-10 09:21:33 +0000

    j'ai corrige mon erreur sur le code

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,7 +14,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (binary_tree_is_full(tree) == 0)
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	if (tree->left == NULL || tree->right == NULL)
 	{
 		return (0);
 	}
@@ -22,6 +26,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	h_d = binary_tree_height(tree->right);
 
 	if (h_d != h_g)
+	{
+		return (0);
+	}
+	if (binary_tree_is_perfect(tree->left) == 0)
+	{
+		return (0);
+	}
+	if (binary_tree_is_perfect(tree->right) == 0)
 	{
 		return (0);
 	}
